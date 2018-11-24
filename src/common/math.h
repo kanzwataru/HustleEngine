b676d6912e49c21d5b46be096383187f6cde6975
Wataru Kanzaki <kanzwataru@gmail.com>
2018-11-24 10:16:13 -0500

    * branchless MIN

--- src/common/math.h
+++ src/common/math.h
@@ -2,7 +2,7 @@
 #define MATH_H
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MIN(a, b) ((b) ^ (((a) ^ (b)) & -((a) < (b))))
 #define CLAMP(val, min, max) (((val) > (max)) ? (max) : ((val < min)) ? (min) : (val))
 #define SGN(a) ((a) > 0) - ((a) < 0)
 #define LERP(a, b, t) ((a) + ((t) * ((b) - (a))))
