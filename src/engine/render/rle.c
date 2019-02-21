c5c122c6aefe2dabc01bf3f2f4a7b189b071eac2
神崎航 <kanzwataru@gmail.com>
2019-02-21 10:11:21 -0500

    * move all logic into macro to deduplicate

--- src/engine/render/rle.c
+++ src/engine/render/rle.c
@@ -8,6 +8,82 @@
 
 #define SET_SIZE(rleptr, rlesize) (GET_RLE_SIZE(rleptr) = (rlesize))
 
+/*
+ * Massive macro that does the RLE drawing.
+ * All the RLE drawing functions share 99% of the procedure so
+ * a macro is the only way!!
+*/
+#define DO_RLE_DRAW(COND, COL) \
+    int pcount, left, right;    \
+    byte lines, lineskip;         \
+                                    \
+    if(OFFSCREEN(rect)) return;         \
+                                                \
+    ++rle; /* skip size header */                                   \
+    dest += CALC_OFFSET(MAX(rect.x, 0), rect.y);                                \
+    lines = (rect.y + rect.h) > SCREEN_HEIGHT ? SCREEN_HEIGHT - rect.y : rect.h;  \
+    lineskip = rect.y < 0 ? abs(rect.y) : 0;                                       \
+                                                                                    \
+    right = (rect.x + rect.w) > SCREEN_WIDTH ? SCREEN_WIDTH - rect.x : rect.w;      \
+    left = rect.x < 0 ? rect.x : 0; /* this is negative! */                         \
+    right += left; /* either does nothing, or subtracts */                          \
+                                                                                    \
+    /* fast-forward RLE to skip the clipped lines */                                \
+    while(lineskip != 0) {                                                         \
+        pcount = 0;                                                               \
+        while(pcount < rect.w) {                                                \
+            pcount += rle->length;                                         \
+            ++rle;                                                  \
+        }                                                       \
+        --lineskip;                                            \
+        --lines;                                              \
+        dest += SCREEN_WIDTH;                                \
+    }                                                       \
+    /* draw */                                              \
+    while(lines != 0) {                                     \
+    pcount = left; /* 0 if no clip, negative if clipping */ \
+                                                            \
+    while(pcount < 0) {                                     \
+        pcount += rle->length;                              \
+        if(pcount > 0) {                                    \
+            if(COND)   /* skip transparent pixels */ \
+              _fmemset(dest, COL, pcount);             \
+            ++rle;                                          \
+            break;                                          \
+        }                                                   \
+                                                            \
+        ++rle;                                              \
+    }                                                       \
+                                                            \
+    if(left != 0) {                                         \
+        while(pcount < right) { /* we've already fast-forwarded so draw to the end of the rle texture */    \
+            if(COND)   /* skip transparent pixels */   \
+              _fmemset(dest + pcount, COL, MIN(rle->length, MAX(0, right - pcount))); \
+            pcount += rle->length;                          \
+            ++rle;                                          \
+        }                                                   \
+    }                                                       \
+    else {                                                  \
+        while(pcount < rect.w) { /* we started at the beginning so we have to fast-forward to the end once we hit the right clip */ \
+            if(COND)   /* skip transparent pixels */   \
+              _fmemset(dest + pcount, COL, MIN(rle->length, MAX(0, right - pcount)));  \
+            pcount += rle->length;                          \
+            ++rle;                                          \
+        }                                                   \
+    }                                                       \
+                                                            \
+    dest += SCREEN_WIDTH;                                   \
+    --lines;                                                \
+}
+
+/* ***************** */
+/* ***************** */
+/* ***************** */
+/* ***************** */
+/* ***************** */
+/* ***************** */
+
+
 size_t buffer_to_rle(RLEImage *rle, buffer_t *buf, int width, int height)
 {
     uint16 offset = 0, line = 0;
@@ -50,198 +126,15 @@ size_t buffer_to_rle(RLEImage *rle, buffer_t *buf, int width, int height)
 
 void draw_rle(buffer_t *dest, const RLEImage *rle, Rect rect)
 {
-    int pcount, left, right;
-    byte lines, lineskip;
-
-    if(OFFSCREEN(rect)) return;
-
-    ++rle; /* skip size header */
-    dest += CALC_OFFSET(MAX(rect.x, 0), rect.y);
-    lines = (rect.y + rect.h) > SCREEN_HEIGHT ? SCREEN_HEIGHT - rect.y : rect.h;
-    lineskip = rect.y < 0 ? abs(rect.y) : 0;
-
-    right = (rect.x + rect.w) > SCREEN_WIDTH ? SCREEN_WIDTH - rect.x : rect.w;
-    left = rect.x < 0 ? rect.x : 0; /* this is negative! */
-    right += left; /* either does nothing, or subtracts */
-
-    /* fast-forward RLE to skip the clipped lines */
-    while(lineskip != 0) {
-        pcount = 0;
-        while(pcount < rect.w) {
-            pcount += rle->length;
-            ++rle;
-        }
-        --lineskip;
-        --lines;
-        dest += SCREEN_WIDTH;
-    }
-
-    /* draw */
-    while(lines != 0) {
-        pcount = left; /* 0 if no clip, negative if clipping */
-
-        while(pcount < 0) {
-            pcount += rle->length;
-            if(pcount > 0) {
-                if(rle->col != 0)   /* skip transparent pixels */
-                  _fmemset(dest, rle->col, pcount);
-                ++rle;
-                break;
-            }
-
-            ++rle;
-        }
-
-        if(left != 0) {
-            while(pcount < right) { /* we've already fast-forwarded so draw to the end of the rle texture */
-                if(rle->col != 0)   /* skip transparent pixels */
-                  _fmemset(dest + pcount, rle->col, MIN(rle->length, MAX(0, right - pcount)));
-                pcount += rle->length;
-                ++rle;
-            }
-        }
-        else {
-            while(pcount < rect.w) { /* we started at the beginning so we have to fast-forward to the end once we hit the right clip */
-                if(rle->col != 0)   /* skip transparent pixels */
-                  _fmemset(dest + pcount, rle->col, MIN(rle->length, MAX(0, right - pcount)));
-                pcount += rle->length;
-                ++rle;
-            }
-        }
-
-        dest += SCREEN_WIDTH;
-        --lines;
-    }
+    DO_RLE_DRAW(rle->col != 0, rle->col);
 }
 
 void draw_rle_filled(buffer_t *dest, const RLEImage *rle, Rect rect, byte col)
 {
-    int pcount, left, right;
-    byte lines, lineskip;
-
-    if(OFFSCREEN(rect)) return;
-
-    ++rle; /* skip size header */
-    dest += CALC_OFFSET(MAX(rect.x, 0), rect.y);
-    lines = (rect.y + rect.h) > SCREEN_HEIGHT ? SCREEN_HEIGHT - rect.y : rect.h;
-    lineskip = rect.y < 0 ? abs(rect.y) : 0;
-
-    right = (rect.x + rect.w) > SCREEN_WIDTH ? SCREEN_WIDTH - rect.x : rect.w;
-    left = rect.x < 0 ? rect.x : 0; /* this is negative! */
-    right += left; /* either does nothing, or subtracts */
-
-    /* fast-forward RLE to skip the clipped lines */
-    while(lineskip != 0) {
-        pcount = 0;
-        while(pcount < rect.w) {
-            pcount += rle->length;
-            ++rle;
-        }
-        --lineskip;
-        --lines;
-        dest += SCREEN_WIDTH;
-    }
-
-    /* draw */
-    while(lines != 0) {
-        pcount = left; /* 0 if no clip, negative if clipping */
-
-        while(pcount < 0) {
-            pcount += rle->length;
-            if(pcount > 0) {
-                if(rle->col != 0)   /* skip transparent pixels */
-                  _fmemset(dest, col, pcount);
-                ++rle;
-                break;
-            }
-
-            ++rle;
-        }
-
-        if(left != 0) {
-            while(pcount < right) { /* we've already fast-forwarded so draw to the end of the rle texture */
-                if(rle->col != 0)   /* skip transparent pixels */
-                  _fmemset(dest + pcount, col, MIN(rle->length, MAX(0, right - pcount)));
-                pcount += rle->length;
-                ++rle;
-            }
-        }
-        else {
-            while(pcount < rect.w) { /* we started at the beginning so we have to fast-forward to the end once we hit the right clip */
-                if(rle->col != 0)   /* skip transparent pixels */
-                  _fmemset(dest + pcount, col, MIN(rle->length, MAX(0, right - pcount)));
-                pcount += rle->length;
-                ++rle;
-            }
-        }
-
-        dest += SCREEN_WIDTH;
-        --lines;
-    }
+    DO_RLE_DRAW(rle->col != 0, col);
 }
 
 void draw_rle_filled_reverse(buffer_t *dest, const RLEImage *rle, Rect rect, byte col)
 {
-    int pcount, left, right;
-    byte lines, lineskip;
-
-    if(OFFSCREEN(rect)) return;
-
-    ++rle; /* skip size header */
-    dest += CALC_OFFSET(MAX(rect.x, 0), rect.y);
-    lines = (rect.y + rect.h) > SCREEN_HEIGHT ? SCREEN_HEIGHT - rect.y : rect.h;
-    lineskip = rect.y < 0 ? abs(rect.y) : 0;
-
-    right = (rect.x + rect.w) > SCREEN_WIDTH ? SCREEN_WIDTH - rect.x : rect.w;
-    left = rect.x < 0 ? rect.x : 0; /* this is negative! */
-    right += left; /* either does nothing, or subtracts */
-
-    /* fast-forward RLE to skip the clipped lines */
-    while(lineskip != 0) {
-        pcount = 0;
-        while(pcount < rect.w) {
-            pcount += rle->length;
-            ++rle;
-        }
-        --lineskip;
-        --lines;
-        dest += SCREEN_WIDTH;
-    }
-
-    /* draw */
-    while(lines != 0) {
-        pcount = left; /* 0 if no clip, negative if clipping */
-
-        while(pcount < 0) {
-            pcount += rle->length;
-            if(pcount > 0) {
-                if(rle->col == 0)   /* skip transparent pixels */
-                  _fmemset(dest, col, pcount);
-                ++rle;
-                break;
-            }
-
-            ++rle;
-        }
-
-        if(left != 0) {
-            while(pcount < right) { /* we've already fast-forwarded so draw to the end of the rle texture */
-                if(rle->col == 0)   /* skip transparent pixels */
-                  _fmemset(dest + pcount, col, MIN(rle->length, MAX(0, right - pcount)));
-                pcount += rle->length;
-                ++rle;
-            }
-        }
-        else {
-            while(pcount < rect.w) { /* we started at the beginning so we have to fast-forward to the end once we hit the right clip */
-                if(rle->col == 0)   /* skip transparent pixels */
-                  _fmemset(dest + pcount, col, MIN(rle->length, MAX(0, right - pcount)));
-                pcount += rle->length;
-                ++rle;
-            }
-        }
-
-        dest += SCREEN_WIDTH;
-        --lines;
-    }
+    DO_RLE_DRAW(rle->col == 0, col);
 }
