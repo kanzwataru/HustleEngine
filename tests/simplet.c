200f2652604b4e3138f500dbe1fb52a59c2d8e36
神崎航 <kanzwataru@gmail.com>
2019-05-04 09:53:31 -0400

    * back to the basics, putting it together slowly

--- tests/simplet.c
+++ tests/simplet.c
@@ -1,72 +1,113 @@
-#include "common/platform.h"
-#include "engine/core.h"
-#include "engine/render.h"
+// #include "common/platform.h"
+// #include "engine/core.h"
+// #include "engine/render.h"
+//
+// #include "platform/video.h"
+//
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+//
+// static RenderData *rd;
+// static memid_t tblock = 0;
+// static void far *transientmem = NULL;
+//
+// static volatile unsigned char *vga = (unsigned char *)0xA0000;
+//
+// static bool input(void)
+// {
+//     //if(keyboard_os_quit_event())
+//     //    return false;
+//
+//     return true;
+// }
+//
+// static void update(void)
+// {
+//     //printf("%d\n", rand());
+// }
+//
+// static void render(void)
+// {
+//     int i;
+//     //renderer_start_frame(rd);
+//     /*
+//     for(i = 0; i < 320 * 200; ++i) {
+//         vga[i] = 6;
+//     } */
+//     memset(vga, 4, 320 * 200);
+// }
+//
+// static void quit(void)
+// {
+//     //renderer_quit(rd, true);
+//     free(transientmem);
+// }
+//
+// static void render_flip(void)
+// {
+//     //renderer_finish_frame(rd);
+// }
+//
+// void simpletest_start(void)
+// {
+//     //int i;
+//     //byte palette[256];
+//
+//     //engine_init();
+//     //video_init_mode(VIDEO_MODE_LOW256, 1);
+//     _asm {
+//         pusha
+//         mov al, 0x13
+//         int 0x10
+//         popa
+//     }
+//     //transientmem = malloc(64000);
+//
+//     //srand(0);
+//     /*
+//     for(i = 0; i < 256; ++i) {
+//         palette[i] = rand();
+//     }
+// */
+//     //rd = renderer_init(transientmem, 0, 0, 0);
+//
+//     memset((void *)0xA0000, 5, 320 * 200);
+//
+//     while(1) {
+//         //update();
+//         render();
+//         //render_flip();
+//     }
+//
+//     //engine_quit();
+//
+//     //quit();
+// }
 
 #include <stdio.h>
+#include <dos.h>
+#include <conio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
-static RenderData *rd;
-static memid_t tblock = 0;
-static void far *transientmem = NULL;
+void main(void) {
+    getch();
 
-static bool input(void)
-{
-    if(keyboard_os_quit_event())
-        return false;
-
-    return true;
-}
-
-static void update(void)
-{
-    printf("%d\n", rand());
-}
-
-static void render(void)
-{
-    int i;
-    renderer_start_frame(rd);
-
-    for(i = 0; i < 320 * 200; ++i) {
-        rd->screen[i] = rand();
-    }
-}
-
-static void quit(void)
-{
-    renderer_quit(rd, true);
-    mem_free_block(tblock);
-}
-
-static void render_flip(void)
-{
-    renderer_finish_frame(rd);
-}
-
-void simpletest_start(void)
-{
-    int i;
-    byte palette[256];
-
-    engine_init();
-    tblock = mem_alloc_block(MEMSLOT_RENDERER_TRANSIENT);
-    transientmem = mem_slot_get(tblock);
-
-    srand(time(NULL));
-    for(i = 0; i < 256; ++i) {
-        palette[i] = rand();
+    _asm {
+        pusha
+        mov al, 0x13
+        int 0x10
+        popa
     }
 
-    rd = renderer_init(transientmem, 0, 0, palette);
+    //memset((void *)0xA0000, 5, 320 * 200);
+    getch();
 
-    while(input()) {
-        update();
-        render();
-        render_flip();
+    _asm {
+        pusha
+        mov al, 0x03
+        int 0x10
+        popa
     }
-
-    engine_quit();
-
-    quit();
 }
