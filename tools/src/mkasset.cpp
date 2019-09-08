3ded213ac9a5ea7f00552b56d33f2c2f5f1d26fd
神崎航 <kanzwataru@gmail.com>
2019-09-08 13:39:38 -0400

    * add basic scaffolding for tools

---
+++ tools/src/mkasset.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include "asset/convert.hpp"
+
+int main(int argc, char **argv)
+{
+    std::printf("Hello from mkasset!\n");
+}
---
+++ tools/src/mkmak.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main(int argc, char **argv)
+{
+    std::printf("Hello from mkmak!\n");
+}
