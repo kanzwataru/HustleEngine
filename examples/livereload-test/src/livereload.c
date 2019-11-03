ce57e6d26756a1c319f6c3df6647e4b69de908f7
神崎航 <kanzwataru@gmail.com>
2019-11-03 00:30:53 -0400

    * made the Squaremark

--- examples/livereload-test/src/livereload.c
+++ examples/livereload-test/src/livereload.c
@@ -4,13 +4,27 @@
 #include "engine/render.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define DO_CUBE_TEST 1
+#define CUBE_MAX     512
+#define CUBE_DELAY   200
+
+#define RAND_RANGE(_min, _max) ((rand() % ((_max) - (_min) + 1)) + (_min))
+
+struct Cube {
+    bool  showing;
+    Rect  rect;
+    Point dir;
+};
 
 struct GameData {
     struct Game *game;
 
-    Rect a;
     int  counter;
-    Point dir;
+    int  cube_counter;
+    int  cube_num;
+    struct Cube cubes[CUBE_MAX];
 };
 
 static const buffer_t palette[] = {
@@ -21,42 +35,69 @@ static const buffer_t palette[] = {
 static struct GameData *g;
 
 void init(void) {
-    g->a.x = 20;
-    g->a.y = 40;
-    g->a.w = 64;
-    g->a.h = 64;
+    int i = 0;
+
+    for(i = 0; i < CUBE_MAX; ++i) {
+        g->cubes[i].rect.x = RAND_RANGE(0, 128);
+        g->cubes[i].rect.y = RAND_RANGE(0, 64);
+        g->cubes[i].rect.w = 16;
+        g->cubes[i].rect.h = 16;
+
+        g->cubes[i].dir.x = RAND_RANGE(0, 1) ? -1 : 1;
+        g->cubes[i].dir.y = RAND_RANGE(0, 1) ? -1 : 1;
+    }
+
+    g->cubes[0].showing = true;
 
     g->counter = 0;
-
-    g->dir.x = 1;
-    g->dir.y = 1;
-
+    g->cube_counter = 0;
+    g->cube_num = 1;
 }
 
 void input(void) {
 }
 
 void update(void) {
-    g->a.x += g->dir.x;
-    g->a.y += g->dir.y;
+    int i;
+    struct Cube *c;
 
-    if(g->a.x > 320 - g->a.w)
-        g->dir.x = -1;
-    if(g->a.x < 0)
-        g->dir.x = 1;
-    if(g->a.y > 200 - g->a.h)
-        g->dir.y = -1;
-    if(g->a.y < 0)
-        g->dir.y = 1;
+#if DO_CUBE_TEST
+    if(g->cube_counter++ > CUBE_DELAY) {
+        g->cube_counter = 0;
+        g->cube_num *= 2;
+        if(g->cube_num > CUBE_MAX)
+            g->cube_num = CUBE_MAX;
+    }
+#endif
+
+    for(i = 0; i < g->cube_num; ++i) {
+        c = &g->cubes[i];
+
+        c->rect.x += c->dir.x;
+        c->rect.y += c->dir.y;
+
+        if(c->rect.x > 320 - c->rect.w)
+            c->dir.x = -1;
+        if(c->rect.x < 0)
+            c->dir.x = 1;
+        if(c->rect.y > 200 - c->rect.h)
+            c->dir.y = -1;
+        if(c->rect.y < 0)
+            c->dir.y = 1;
+    }
 
     //printf("%d %d\n", g->a.x, g->a.y);
 }
 
 void render(void) {
-    renderer_set_palette(palette, 0, 2);
+    int i = 0;
 
+    renderer_set_palette(palette, 0, 2);
     renderer_clear(0);
-    renderer_draw_rect(0, g->a, 1);
+
+    for(i = 0; i < g->cube_num; ++i) {
+        renderer_draw_rect(0, g->cubes[i].rect, 1);
+    }
 
     renderer_flip();
 }
